b17ad50cd0adc0dd94ec030690c9ddcb5bb70fc8
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-06-27 21:45:52 +0300

    a

--- Git/JavaScript/01/index.js
+++ Git/JavaScript/01/index.js
@@ -2,6 +2,59 @@
 
 document.write("Merhaba Dünya");
 
-for (const iterator of object) {
-    
-}
+let price = 111
+let stringPrice = "111"
+let hasPassword = true
+
+console.log(
+    "price: ",
+    typeof(price)
+)
+
+console.log(
+    "stringPrice: ",
+    typeof(stringPrice)
+)
+
+console.log(
+    "hasPassword: ",
+    typeof(hasPassword)
+)
+
+// string bilgileri int ve float'a donüştürmek
+let number1 = "11"
+console.log("number1: ", parseInt(number1))
+
+
+// string bilgileri int ve float'a donüştürmek
+let number2 = "12px"
+number2 = parseInt(number2)
+console.log("number2: ", number2, typeof(number2))
+
+// dönüşüm kabul edilmez (nan)
+let number3 = "px12px"
+number3 = parseInt(number3)
+console.log("number3: ", number3, typeof(number3))
+//Number()
+let number4 = "12"
+number4 = Number(number4)
+console.log("number4: ", number4, typeof(number4))
+
+let number5 = "12px"
+number5 = Number(number5)
+console.log("number5: ", number5, typeof(number5))
+
+//Float
+let float1 = "12.1"
+float1 = Number(float1)
+console.log("float1: ", float1, typeof(float1))
+
+let number6 = "12.7"
+number6 = parseFloat(number6)
+console.log("float number: ", number6, typeof(number6))
+
+
+//Number to string
+let numberToString = 12345
+numberToString = numberToString.toString()
+console.log(numberToString, typeof(numberToString))
