d100eb0e33ad3903fc6febbb49992e0afe6cdd2c
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-07-04 21:50:50 +0300

    kodluyoruz

--- Git/JavaScript/02/index.js
+++ Git/JavaScript/02/index.js
@@ -228,7 +228,7 @@ var canBuy =
 
 console.log(canBuy)   
 */
-
+/*
 const SMILE = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
 <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
@@ -275,3 +275,76 @@ if (examGrade >= 0 && examGrade <= 100) {
 
 
 info.innerHTML = `${textInfo} -> ${examGrade}`
+*/
+
+// function printHello(name){
+//     console.log("Hello, World! "+ name)
+// }
+// printHello("Cihan")
+
+// let sayi1 = 6
+// let sayi2 = 2
+// function addition(){
+//     let sayi3 = 3
+//     return sayi1 + sayi2 + sayi3
+// }
+
+// function multiple(){
+//     return sayi1 * sayi3
+//     //Burada hata alıyoruz: bunun nedeni başka bir fonksiyonun içinde tanımlı olan sayi3 lokal değişkenini kapsamı dışında 
+// //kullanmaya çalışmamız. sayi1 değişkeni ise global olduğundan burada kullanılabilir
+// }
+
+
+/*
+function daireAlaniHesaplama (r, PI = 3.14) 
+{ 
+    return PI*r*r;		
+}
+var donenSonuc = daireAlaniHesaplama(3); 
+console.log(donenSonuc); // 28.25999999 sonucunu göreceğiz.
+
+*/
+
+
+//arrow this is a sorrow
+// dikkat edilmesi gerekilen durum ise eğer arrow fonksiyonunun içinde
+// süslü parantez var ise return yazılmalıdır.
+
+/*
+const myList = (param1, param2) => param1.concat(param2)
+
+var feedTheCat = (cat) => {
+    if (cat === "hungry"){
+        return 'Feed the cat'
+    } else {
+        return "Do not feed the cat"
+    }
+}
+
+*/
+
+/*
+ this'in arrow fonksiyonlardaki davranışı,regular fonksiyonlardaki
+  davranışından farklıdır.Nasıl ve nerede oluşturulursa oluşturulsun 
+  this'in arrow fonksiyonu içerisindeki değeri her zaman parent 
+  fonksiyonuna eşittir.Diğer bir deyişle arrow fonksiyonu kendi 
+  execution context'ini oluşturmaz. Yani kendisini referans göstermez, 
+  her zaman parent'ına bakar.
+*/
+
+let movie = { 
+
+    name: "La la land",
+    
+    thisInArrow:() => { 
+    console.log("Movie name is " + this.name); // 'this' window'u referans gösterir. Bu yüzden name'yi bulamaz.
+    }, 
+    
+    thisInRegular(){ 
+    console.log("Movie name is " + this.name); // 'this' kendisini referans gösterir ve çalışır.
+    } 
+    
+};
+movie.thisInArrow(); // output : Movie name is
+movie.thisInRegular(); // output : Movie name is La la land
---
+++ Git/JavaScript/03/index.js
@@ -0,0 +1,72 @@
+/*
+const seriesList = list => {
+    list.forEach((series, index) => {
+        console.log(`${index+1}. ${series}`)
+    })
+}
+
+seriesList(["Firefly","The Mandalorian","Breaking Bad"])
+
+
+const newArray = (nums) => {
+    const newNums = nums.map(e => {
+        if(e%2==0){
+            return e*2
+        } else if(e%2==1){
+            return e*3
+        }
+    })
+    return newNums
+}
+
+console.log(newArray([1,2,3,4,5]))
+*/
+
+/***************************************DOM EVENTS**********************************/
+
+// const color = document.querySelector("#select")
+// color.addEventListener('change', selectBox)
+
+// function selectBox(event){
+//     console.log("Etkinlik tipi : " + event.type)
+//     console.log("deger : " + event.target.value)
+// }
+
+/*
+const selectBox = document.querySelector("#selectColor")
+const button = document.querySelector("#button")
+const form = document.querySelector(".inputText")
+const cerceve = document.querySelector(".container")
+
+selectBox.addEventListener('click', function(){
+    console.log("selectBox")    
+})
+
+form.addEventListener('click', function(){
+    console.log("form")    
+})
+
+cerceve.addEventListener('click', function(){
+    console.log("cerceve")    
+})
+*/
+
+/**********************Function Bölüm Sonu Egzersizi******************/ 
+
+let counter = 0
+let counterDOM = document.querySelector('#counter')
+let increaseDOM = document.querySelector('#increase')
+let decreaseDOM = document.querySelector('#decrease')
+
+counterDOM.innerHTML = counter
+
+ 
+function clickEvent(){
+    console.log(this.id)
+    this.id === "increase" ? counter+= 1 : counter-= 1
+    
+    counterDOM.innerHTML = counter    
+}
+
+decreaseDOM.addEventListener("click", clickEvent)
+increaseDOM.addEventListener("click", clickEvent)
