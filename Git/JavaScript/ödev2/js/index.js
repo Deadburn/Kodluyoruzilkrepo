a7a2e5845b4a357404abd7bfb4368b085be816ba
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-07-22 00:01:45 +0300

    Kodluyoruz

--- Git/JavaScript/ödev2/js/index.js
+++ Git/JavaScript/ödev2/js/index.js
@@ -1,16 +1,15 @@
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
+let yerel = JSON.parse(localStorage.getItem('LocalList'))
+console.log(yerel)
+
+let tag = []
+
 
 
 let tagName = document.getElementsByTagName("li")
 for(let i = 0; i<tagName.length; i++) {
+    if(yerel) {
+        tagName[i].innerHTML = yerel[i]
+    } 
     let span = document.createElement("span")
     let txt = document.createTextNode("\u00D7")
     span.className = "close"
@@ -23,6 +22,7 @@ for(let i = 0; i < close.length; i++){
     close[i].onclick = function() {
         let div = this.parentElement
         div.style.display = "none";
+        localLi()
     }
 }
 
@@ -43,7 +43,7 @@ list.addEventListener(
       let listItem = document.createElement("li")
       let inputV = document.getElementById("task").value
       let t = document.createTextNode(inputV)
-
+      
       listItem.appendChild(t)
         // space escape veya input'un boş olma durumu
       if(inputV === "" || inputV.replace(/^\s+|\s+$/g, "").length == 0){
@@ -51,6 +51,7 @@ list.addEventListener(
       } else {
           $(".success").toast("show")
           document.getElementById("list").appendChild(listItem)
+          localLi()
       }
       document.getElementById("task").value = ""
       let span = document.createElement("span")
@@ -60,12 +61,36 @@ list.addEventListener(
       span.appendChild(txt)
       listItem.appendChild(span)
       
+      
       for (i = 0; i< close.length; i++) {
           close[i].onclick = function () {
               let div = this.parentElement
               div.style.display = "none"
           }
+          
       }
+      localLi()
     }
 
+
+
+//localStorage.setItem("ListItems", tagName)
+function localLi(){
+    
+    for(let i = 0; i<tagName.length; i++) {
+        tagName[i].style.display == "none" ? tag = tag.filter(item => item !== tagName[i].innerHTML) :
+         tag[i] = tagName[i].innerHTML
+         
+     }
+     console.log(tag) 
+    localStorage.setItem("LocalList", JSON.stringify(tag))
+}
+
+localLi()
+
+// for(let i = 0; i<tagName.length; i++) {
+//     console.log(tagName[i].innerHTML)
+//     tagName[i].innerHTML = tag[i]
+    
+// } 
 
