d4eea4de9ea0619da527cace90152513dbf1f039
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-07-22 00:19:33 +0300

    Commit

---
+++ Git/JavaScript/object/index.js
@@ -0,0 +1,21 @@
+console.log("Nerde lan beynin")
+
+let arrayObj = [1, 2, 3]
+let object1 = { obj: 1 }
+
+console.log("arrayObj", typeof (arrayObj))
+console.log("object1", typeof (object1))
+
+let item1 = new Object()
+let item2 = {}
+console.log("item1", typeof (item1))
+console.log("item2", typeof (item2))
+
+// obje oluşturmak
+let item3 = {}
+let item4 = new Object()
+
+//console.log(object1)
+
+//let object1_1 = Object.create(object1)
+//console.log(object1_1.obj)
