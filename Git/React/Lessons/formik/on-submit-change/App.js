9c4fb976af166553f59f2371e12a42014d2a3f8f
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-08-05 18:29:10 +0300

    kodluyoruz-formik

---
+++ Git/React/Lessons/formik/on-submit-change/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+
+import { Formik } from "formik";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {
+          ({handleSubmit, handleChange}) => (
+            <form onSubmit={handleSubmit}>
+          <label htmlFor="firstName">First Name:</label>
+          <input name="firstName" onChange={handleChange} />
+
+          <br />
+          <br />
+
+          <label htmlFor="lastName">Last Name:</label>
+          <input name="lastName" onChange={handleChange} />
+
+          <br />
+          <br />
+
+          <label htmlFor="email">Email:</label>
+          <input
+            name="email"
+            onChange={handleChange}
+            type="email"
+          />
+
+          <br />
+          <br />
+
+          <button type="submit">Submit</button>
+        </form>
+          )
+        }
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
