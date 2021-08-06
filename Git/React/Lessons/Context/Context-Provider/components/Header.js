c042e5f3af9d18adec7c52893eb34f02efb65da9
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-08-06 21:56:48 +0300

    context

---
+++ Git/React/Lessons/Context/Context-Provider/components/Header.js
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react'
+
+import ThemeContext from "../context/ThemeContext";
+
+function Header() {
+    const { theme, setTheme } = useContext(ThemeContext);
+    return (
+        <div>
+            Active Theme: {theme}
+
+            <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>Change Theme</button>
+        </div>
+    )
+}
+
+export default Header
---
+++ Git/React/Lessons/Context/giriş/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import ThemeContext from "./context/ThemeContext";
+import Button from "./components/Button";
+
+
+function App() {
+
+  return (
+      <ThemeContext.Provider value="dark">
+        <Button />
+      </ThemeContext.Provider>
+  );
+}
+
+
+
+
+export default App;
