79dc0dca5709cb789aca3307e3c8e5843e50be1d
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-08-10 22:06:44 +0300

    test

---
+++ Git/React/test/src/components/Todo/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const defaultItems = [
+  {
+    name: "Item A",
+  },
+  {
+    name: "Item B",
+  },
+  {
+    name: "Item C",
+  },
+];
+
+function Todo() {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState(defaultItems);
+  return (
+    <div>
+      <label >
+        Text
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </label>
+
+      <button
+        onClick={() => {
+          setItems((prev) => [...prev, { name: text }]);
+          setText("");
+        }}
+      >
+        Add
+      </button>
+
+      <br />
+      <br />
+      {items.map((item, key) => (
+        <div key={key}>{item.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Todo;
