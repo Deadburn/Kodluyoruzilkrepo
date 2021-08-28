4c4532cf49c6df71c3230e31c6d32b533140b510
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-08-28 22:54:39 +0300

    todos

--- Git/Redux/todo-app/src/components/TodoList.js
+++ Git/Redux/todo-app/src/components/TodoList.js
@@ -1,26 +1,34 @@
 import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy, selectFilteredTodos} from "../redux/todos/todosSlice";
 
 function TodoList() {
   const dispatch = useDispatch();
-  const items = useSelector((state) => state.todos.items);
-  console.log(items);
+  const filteredTodos = useSelector(selectFilteredTodos);
+  
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure ?")) {
+      dispatch(destroy(id));
+    }
+  };
 
   return (
     <ul className="todo-list">
-      {items.map((item) => (
+      {filteredTodos.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
             <input
               className="toggle"
               type="checkbox"
               checked={item.completed}
-              onChange={() => dispatch(toggle({ id: item.id}))}
+              onChange={() => dispatch(toggle({ id: item.id }))}
             />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
--- Git/Redux/todo-app/src/redux/todos/todosSlice.js
+++ Git/Redux/todo-app/src/redux/todos/todosSlice.js
@@ -15,6 +15,7 @@ export const todosSlice = createSlice({
         completed: false,
       },
     ],
+    activeFilter: 'all',
   },
   reducers: {
     addTodo: (state, action) => {
@@ -25,8 +26,30 @@ export const todosSlice = createSlice({
       const item = state.items.find((item) => item.id === id);
       item.completed = !item.completed;
     },
+    destroy: (state, action) => {
+      const id = action.payload;
+      const filtered = state.items.filter((item) => item.id !== id);
+      state.items = filtered;
+    },
+    changeActiveFilter: (state, action) => {
+      state.activeFilter = action.payload;
+    },
+    clearCompleted: (state) => {
+      const filtered = state.items.filter((item) => item.completed === false)
+      state.items = filtered;
+    },
   },
 });
 
-export const { addTodo, toggle } = todosSlice.actions;
+export const selectTodos = (state) => state.todos.items;
+export const selectFilteredTodos = (state) => {
+  if(state.todos.activeFilter === 'all'){
+    return state.todos.items;
+  }
+  return state.todos.items.filter((todo) => 
+    state.todos.activeFilter === 'active' ? todo.completed === false : todo.completed === true
+  )
+}
+export const selectActiveFilter = (state) => state.todos.activeFilter;
+export const { addTodo, toggle, destroy, changeActiveFilter, clearCompleted } = todosSlice.actions;
 export default todosSlice.reducer;
