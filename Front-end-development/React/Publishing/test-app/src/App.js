cea927285e89bf4e8407984f60cae775bef3c67c
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-12-28 18:05:13 +0300

    kodluyoruz

---
+++ Front-end-development/React/Publishing/test-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import {Button, Paragraph} from 'deadburn-package'
+import 'deadburn-package/dist/index.css'
+
+function App() {
+  return (
+    <div className="App">
+        <Paragraph text="minor değişiklik yapıldı" />
+        <Button text="click" />
+    </div>
+  );
+}
+
+export default App;
