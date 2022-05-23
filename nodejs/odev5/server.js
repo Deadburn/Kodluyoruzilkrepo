ad96630f92187a6a73bfb3f1dec4e366bd1f6f66
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-05-23 13:45:48 +0300

    Nodejs ödev5

---
+++ nodejs/odev5/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("Server oluşturuldu")
+
+    const url = req.url;
+
+    if(url === "/") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h2>Anasayfa</h2>")
+    } else if(url === "/hakkimda") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h2>hakkimda sayfasina hosgeldiniz</h2>")
+    } else if(url === "/iletisim") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h2>iletisim sayfasina hosgeldiniz</h2>")
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.write("<h2>404 SAYFA BULUNAMADI</h2>");
+    }
+
+    res.end();
+})
+
+const port = 5000
+
+server.listen(port, () => {
+    console.log(`Sunucu port ${port} de başlatıldı.`)
+})
