470d9139b955ed0baa9eb1f09441f3c8b0899ed2
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-07-01 13:25:52 +0300

    subscriptions

--- graphql/graphql-comments/index.js
+++ graphql/graphql-comments/index.js
@@ -1,10 +1,10 @@
-import { ApolloServer, gql } from "apollo-server";
+import { GraphQLServer, withFilter } from "graphql-yoga";
 import { users, posts, comments } from "./data.js";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 //add nanoid package
 import { nanoid } from "nanoid";
+import pubsub from "./pubsub.js";
 
-const typeDefs = gql`
+const typeDefs = `
   # User type definition
   type User {
     id: ID!
@@ -102,22 +102,99 @@ const typeDefs = gql`
     deleteComment(id: ID!): Comment!
     deleteAllComments: DeleteAllOutput!
   }
+
+  type Subscription {
+    userCreated: User!
+    userUpdated: User!
+    userDeleted: User!
+
+    postCreated(user_id: ID): Post!
+    postUpdated: Post!
+    postDeleted: Post!
+    postCount: Int!
+
+    commentCreated(post_id: ID): Comment!
+    commentUpdated: Comment!
+    commentDeleted: Comment!
+  }
+
 `;
 
 const resolvers = {
+  Subscription: {
+    // User
+    userCreated: {
+      subscribe: (_, __, {pubsub}) =>  pubsub.asyncIterator('userCreated')
+    },
+    userUpdated: {
+      subscribe: (_, __, {pubsub}) =>  pubsub.asyncIterator('userUpdated')
+    },
+    userDeleted: {
+      subscribe: (_, __, {pubsub}) =>  pubsub.asyncIterator('userDeleted')
+    },
+
+    // Post
+    postCreated: {
+      subscribe: withFilter(
+        (_, __, {pubsub}) => pubsub.asyncIterator('postCreated'),
+        (payload, variables) => {
+          // console.log("payload", payload);
+          // console.log("variables", variables);
+
+          return variables.user_id ? (payload.postCreated.user_id === variables.user_id) : true;
+
+        },
+      ),
+    },
+    postUpdated: {
+      subscribe: (_, __, {pubsub}) => pubsub.asyncIterator('postUpdated')
+    },
+    postDeleted: {
+      subscribe: (_, __, {pubsub}) => pubsub.asyncIterator('postDeleted')
+    },
+    postCount: {
+      subscribe: (_, __, {pubsub}) => {
+        setTimeout(() => {
+          pubsub.publish('postCount', { postCount: posts.length});
+        })
+
+        return pubsub.asyncIterator('postCount')
+
+      }
+    },
+    
+    // Comment
+    commentCreated: {
+      subscribe: withFilter(
+        (_, __, {pubsub}) => pubsub.asyncIterator('commentCreated'),
+        (payload, variables) => {
+
+          return variables.post_id ? (payload.commentCreated.post_id === variables.post_id) : true
+        }
+      )
+    },
+    commentUpdated: {
+      subscribe: (_, __, {pubsub}) => pubsub.asyncIterator('commentUpdated')
+    },
+    commentDeleted: {
+      subscribe: (_, __, {pubsub}) => pubsub.asyncIterator('commentDeleted')
+    }
+  },
   Mutation: {
     // User
-    createUser: (parent, { data }) => {
+    // Parent'ı kullanmadıgımız için _ diye bir değişken oluşturduk
+    createUser: (_, { data }, { pubsub }) => {
       const user = {
         id: nanoid(),
         ...data,
       };
 
       users.push(user);
+      pubsub.publish('userCreated', { userCreated: user })
 
       return user;
     },
-    updateUser: (parent, { id, data }) => {
+    updateUser: (_, { id, data }, { pubsub}) => {
       const user_index = users.findIndex((user) => user.id === id);
 
       if (user_index === -1) {
@@ -128,10 +205,11 @@ const resolvers = {
         ...users[user_index],
         ...data,
       });
+      pubsub.publish('userUpdated', { userUpdated: updated_user });
 
       return updated_user;
     },
-    deleteUser: (parent, { id }) => {
+    deleteUser: (_, { id }, { pubsub}) => {
       const user_index = users.findIndex((user) => user.id === id);
 
       if (user_index === -1) {
@@ -140,6 +218,8 @@ const resolvers = {
 
       const deleted_user = users[user_index];
       users.splice(user_index, 1);
+
+      pubsub.publish('userDeleted', { userDeleted: deleted_user });
 
       return deleted_user;
     },
@@ -153,17 +233,19 @@ const resolvers = {
     },
 
     // Post
-    createPost: (parent, { data }) => {
+    createPost: (_, { data }, { pubsub }) => {
       const post = {
         id: nanoid(),
         ...data,
       };
 
       posts.push(post);
+      pubsub.publish('postCreated', { postCreated: post});
+      pubsub.publish('postCount', { postCount: posts.length });
 
       return post;
     },
-    updatePost: (parent, { id, data }) => {
+    updatePost: (_, { id, data }, { pubsub }) => {
       const post_index = posts.findIndex((post) => post.id === id);
 
       if (post_index === -1) {
@@ -174,10 +256,11 @@ const resolvers = {
         ...posts[post_index],
         ...data,
       });
+      pubsub.publish('postUpdated', { postUpdated: updated_post });
 
       return updated_post;
     },
-    deletePost: (parent, { id }) => {
+    deletePost: (_, { id }, {pubsub}) => {
       const post_index = posts.findIndex((post) => post.id === id);
 
       if (post_index === -1) {
@@ -186,6 +269,8 @@ const resolvers = {
 
       const deleted_post = posts[post_index];
       posts.splice(post_index, 1);
+      pubsub.publish('postDeleted', { postDeleted: deleted_post });
+      pubsub.publish('postCount', { postCount: posts.length });
 
       return deleted_post;
     },
@@ -193,23 +278,26 @@ const resolvers = {
       const length = posts.length;
       posts.splice(0, length);
 
+      pubsub.publish('postCount', { postCount: posts.length });
+
       return {
         count: length,
       };
     },
 
     // Comment
-    createComment: (parent, { data }) => {
+    createComment: (_, { data }, { pubsub}) => {
       const comment = {
         id: nanoid(),
         ...data,
       };
 
       comments.push(comment);
+      pubsub.publish('commentCreated', { commentCreated: comment });
 
       return comment;
     },
-    updateComment: (parent, { id, data }) => {
+    updateComment: (_, { id, data }, { pubsub }) => {
       const comment_index = comments.findIndex((comment) => comment.id === id);
 
       if (comment_index === -1) {
@@ -220,10 +308,11 @@ const resolvers = {
         ...comments[comment_index],
         ...data,
       });
+      pubsub.publish('commentUpdated', { commentUpdated: updated_comment });
 
       return updated_comment;
     },
-    deleteComment: (parent, { id }) => {
+    deleteComment: (_, { id }, { pubsub}) => {
       const comment_index = comments.findIndex((comment) => comment.id === id);
 
       if (comment_index === -1) {
@@ -232,6 +321,7 @@ const resolvers = {
 
       const deleted_comment = comments[comment_index];
       comments.splice(comment_index, 1);
+      pubsub.publish('commentDeleted', { commentDeleted: deleted_comment });
 
       return deleted_comment;
     },
@@ -274,29 +364,13 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
+
+const server = new GraphQLServer({
   typeDefs,
   resolvers,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground({
-      // options
-    }),
-  ],
+  context: {
+    pubsub,
+  },
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
-
-/*  Hata kontrolü
-
-user: (parent, args) => {
-      const user = users.find((user) => user.id === args.id);
-      if (!user) {
-        return new Error("User not found");
-      }
-
-      return user;
-    },
-    
-*/
+server.start(() => console.log("Server is running on localhost:4000"));
